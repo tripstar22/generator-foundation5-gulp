a6ceff3195f2ede338ebdef6b521aecb488ae48f
Trip Pruitt <trip@brainjocks.com>
2015-12-17 15:04:00 -0500

    added gulp task for vendor js

--- generators/app/templates/gulpfile.js
+++ generators/app/templates/gulpfile.js
@@ -5,7 +5,9 @@ var gulp = require('gulp'),
     livereload = require('gulp-livereload'),
     imagemin = require('gulp-imagemin'),
     autoprefixer = require('gulp-autoprefixer'),
-    rename = require('gulp-rename');
+    rename = require('gulp-rename'),
+    concat = require('gulp-concat'),
+    bowerRoot = 'src/bower-components/';
 
 // sass task
 // compile
@@ -15,20 +17,42 @@ var gulp = require('gulp'),
 gulp.task('styles', function() {
     gulp.src('src/scss/**/*.scss')
         .pipe(plumber())
-        .pipe(sass({outputStyle: 'compressed'}))
-        .pipe(autoprefixer('last 2 versions'))
+         .pipe(sass({}))
+         // add this as option for production for pipe.sass outputStyle: 'compressed'
+         // .pipe(autoprefixer('last 2 versions'))
         .pipe(rename({suffix: '.min'}))
         .pipe(gulp.dest('dist/css'))
+        .pipe(livereload());
+});
+
+// combine vendor scripts
+// uglifies
+// rename with .min
+// *** NOT IN GULP WATCH ***
+// *** HAVE TO RUN 'gulp scriptsVendor' FOR TASK TO WORK ***
+gulp.task('scriptsVendor', function() {
+    gulp.src(
+        [
+            bowerRoot + 'jquery/dist/jquery.min.js'
+        ],
+        {base: 'src/bower-components/'})
+        .pipe(plumber())
+        // uncomment for production
+        //.pipe(uglify())
+        .pipe(concat('dist/js/vendor.js'))
+        .pipe(rename({suffix: '.min'}))
+        .pipe(gulp.dest(''))
         .pipe(livereload());
 });
 
 // custom scripts task
 // uglifies
 // rename with .min
-gulp.task('scripts', function() {
+gulp.task('scriptsCust', function() {
     gulp.src('src/js/*.js')
         .pipe(plumber())
-        .pipe(uglify())
+         // uncomment for production
+         // .pipe(uglify())
         .pipe(rename({suffix: '.min'}))
         .pipe(gulp.dest('dist/js'))
         .pipe(livereload());
@@ -36,7 +60,7 @@ gulp.task('scripts', function() {
 
 // minify images
 gulp.task('images', function() {
-    gulp.src('src/img/*')
+    gulp.src('src/img/*.+(png|jpg|gif|svg)')
         .pipe(imagemin())
         .pipe(gulp.dest('dist/img'));
 });
@@ -52,14 +76,14 @@ gulp.task('watch', function() {
 
     // watch these tasks
     gulp.watch('src/scss/**/*.scss', ['styles']);
-    gulp.watch('src/js/*.js', ['scripts']);
+    gulp.watch('src/js/*.js', ['scriptsCust']);
     gulp.watch('src/img/*', ['images']);
 });
 
-// gulp runs all your tasks
+// gulp runs your tasks
 gulp.task('default', [
     'styles',
-    'scripts',
+    'scriptsCust',
     'images',
     'watch'
 ]);
